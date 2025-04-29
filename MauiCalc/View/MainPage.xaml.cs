4a9f900338afa5e206823e59461a12a18cc6269e
Cat5Dog2 <cat5dog2.tskhrcm@gmail.com>
2025-04-29 14:28:03 +0900

    preparation ViewModel Binding

--- MauiCalc/View/MainPage.xaml.cs
+++ MauiCalc/View/MainPage.xaml.cs
@@ -1,24 +1,14 @@
-﻿namespace MauiCalc.View;
+﻿using MauiCalc.ViewModel;
+namespace MauiCalc.View;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	readonly CalcViewModel _ViewModel = new();
 
 	public MainPage()
 	{
 		InitializeComponent();
-	}
-
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		BindingContext = _ViewModel;
 	}
 }
 
