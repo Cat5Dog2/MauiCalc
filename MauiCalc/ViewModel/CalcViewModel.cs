f06877e4eecff52d6714764a06c5ec42de3ee687
Cat5Dog2 <cat5dog2.tskhrcm@gmail.com>
2025-05-01 14:24:35 +0900

    create calculation processing

--- MauiCalc/ViewModel/CalcViewModel.cs
+++ MauiCalc/ViewModel/CalcViewModel.cs
@@ -2,34 +2,57 @@ using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
+using MauiCalc.Model;
 
 namespace MauiCalc.ViewModel;
 
 public class CalcViewModel : INotifyPropertyChanged
 {
+    // プロパティ変更通知
     public event PropertyChangedEventHandler? PropertyChanged;
     protected void OnPropertyChanged([CallerMemberName] string? name = null) =>
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
 
+    // 計算結果フィールド
     private string _label = "0";
 
+    // 計算結果プロパティ
     public string Label
     {
         get => _label;
         set
         {
             _label = value;
+            // 更新通知
             OnPropertyChanged();
         }
     }
 
+    // 電卓クラスのインスタンス化
+    readonly ClassCalc _calc = new();
+    // ボタンが押された時のコマンド
     public ICommand BtnCommand { get; }
+
     public CalcViewModel()
     {
         BtnCommand = new Command<string>(
             (n) =>
             {
-                this.Label = n;
+                // 現在の数値をプロパティに設定
+                _calc.Display = this.Label;
+                // パラメーターで処理分岐
+                this.Label = n switch
+                {
+                    "C" => _calc.ClrKey(),
+                    "." => _calc.DotKey(),
+                    "/" => _calc.DivisionKey(),
+                    "*" => _calc.MultiKey(),
+                    "+" => _calc.PlusKey(),
+                    "-" => _calc.MinusKey(),
+                    "=" => _calc.EnterKey(),
+                    // その他は数値キー
+                    _ => _calc.NumKey(int.Parse(n)),
+                };
             }
         );
     }
